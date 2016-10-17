c9469103125f454f57e8247e8ae9487468064dcd
chenms-m2 <hellochenms@163.com>
2016-10-17 22:40:32 +0800

    Feature: Init. BugId:/ Description:

---
+++ MHelloGCD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MHelloGCD
+//
+//  Created by chenms on 16/10/17.
+//  Copyright © 2016年 chenms.m2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MHelloGCD
+
+## MHello前缀含义
+- 代码片段集，供开发时直接使用
+
+## MHelloGCD
+- gcd demo
